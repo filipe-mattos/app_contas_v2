54c84666dc748b7227dfce1139cc53a5bdefc0b4
Filipe Magioni Mattos <filipe.mattos@conexos.inf.br>
2022-06-28 19:53:59 -0300

    first commit

---
+++ lib/pages/settings.dart
@@ -0,0 +1,156 @@
+import 'package:app_contas_v1/controllers/db_helper.dart';
+import 'package:app_contas_v1/pages/widgets/confirm_dialog.dart';
+import 'package:flutter/material.dart';
+
+class Settings extends StatefulWidget {
+  const Settings({Key? key}) : super(key: key);
+
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  //
+
+  DbHelper dbHelper = DbHelper();
+
+  //
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        title: Text(
+          "Configurações",
+        ),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(
+          12.0,
+        ),
+        children: [
+          ListTile(
+            onTap: () async {
+              bool answer = await showConfirmDialog(context, "Aviso",
+                  "Não vai ser posssível reverter a ação. Todos os seus dados serão apagados");
+              if (answer) {
+                await dbHelper.cleanData();
+                Navigator.of(context).pop();
+              }
+            },
+            tileColor: Colors.white,
+            contentPadding: EdgeInsets.symmetric(
+              vertical: 12.0,
+              horizontal: 20.0,
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(
+                8.0,
+              ),
+            ),
+            title: Text(
+              "Apagar todas as movimentações",
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+            subtitle: Text(
+              "Essa ação é irreversível",
+            ),
+            trailing: Icon(
+              Icons.delete_forever,
+              size: 32.0,
+              color: Colors.black87,
+            ),
+          ),
+          //
+          SizedBox(
+            height: 20.0,
+          ),
+          //
+          ListTile(
+            onTap: () async {
+              String nameEditing = "";
+              String? name = await showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  backgroundColor: Colors.grey[300],
+                  title: Text(
+                    "Alterar Nome",
+                  ),
+                  content: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white70,
+                      borderRadius: BorderRadius.circular(
+                        12.0,
+                      ),
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      vertical: 8.0,
+                      horizontal: 16.0,
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Seu Nome",
+                        border: InputBorder.none,
+                      ),
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                      maxLength: 12,
+                      onChanged: (val) {
+                        nameEditing = val;
+                      },
+                    ),
+                  ),
+                  actions: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop(nameEditing);
+                      },
+                      child: Text(
+                        "OK",
+                      ),
+                    ),
+                  ],
+                ),
+              );
+              //
+              if (name != null && name.isNotEmpty) {
+                DbHelper dbHelper = DbHelper();
+                await dbHelper.addName(name);
+              }
+            },
+            tileColor: Colors.white,
+            contentPadding: EdgeInsets.symmetric(
+              vertical: 12.0,
+              horizontal: 20.0,
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(
+                8.0,
+              ),
+            ),
+            title: Text(
+              "Mudar Nome",
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+            trailing: Icon(
+              Icons.change_circle,
+              size: 32.0,
+              color: Colors.black87,
+            ),
+          ),
+          //
+          SizedBox(
+            height: 20.0,
+          ),
+        ],
+      ),
+    );
+  }
+}
